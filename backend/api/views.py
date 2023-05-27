864bba6cd1824781914c5d7f36d4f5eb7d5dfa18
Nikita <tt.nikitk@mail.ru>
2023-05-27 13:46:57 +0400

    Add <in> filter

--- backend/api/views.py
+++ backend/api/views.py
@@ -9,14 +9,14 @@ class TaskInWorkListView(generics.ListAPIView):
     queryset = TaskInWork.objects.all()
     serializer_class = TaskInWorkSerializer
     filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
-    filterset_fields = [
-        'type_of_work__local_id',
-        'plan_date_start',
-        'plan_date_end',
-        'fact_date_start',
-        'fact_date_end',
-        'unom'
-    ]
+    filterset_fields = {
+        'type_of_work__local_id': ["in", "exact"],
+        'plan_date_start': ["in", "exact"],
+        'plan_date_end': ["in", "exact"],
+        'fact_date_start': ["in", "exact"],
+        'fact_date_end': ["in", "exact"],
+        'unom': ["in", "exact"]
+    }
 
 
 class ProblemListView(generics.ListAPIView):
@@ -32,14 +32,14 @@ class ObjectListView(generics.ListAPIView):
     queryset = Object.objects.all()
     serializer_class = ObjectSerializer
     filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
-    filterset_fields = [
-        'adm_area',
-        'district',
-        'street',
-        'house',
-        'corpus',
-        'composition',
-        'structure',
-        'col_770__local_id',
-        'col_782',
-    ]
+    filterset_fields = {
+        'adm_area': ["in", "exact"],
+        'district': ["in", "exact"],
+        'street': ["in", "exact"],
+        'house': ["in", "exact"],
+        'corpus': ["in", "exact"],
+        'composition': ["in", "exact"],
+        'structure': ["in", "exact"],
+        'col_770__local_id': ["in", "exact"],
+        'col_782': ["in", "exact"],
+    }
